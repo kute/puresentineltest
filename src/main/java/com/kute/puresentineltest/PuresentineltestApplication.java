c39a3f92d7c03fa9558ba17978f4ec6a62567e35
白龙(23119351) <bailong001@ke.com>
2018-10-06 17:33:31 +0800

    first commit

---
+++ src/main/java/com/kute/puresentineltest/PuresentineltestApplication.java
@@ -0,0 +1,12 @@
+package com.kute.puresentineltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuresentineltestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuresentineltestApplication.class, args);
+	}
+}
